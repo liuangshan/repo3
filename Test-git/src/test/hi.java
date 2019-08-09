665333914bf7778ef4e85d72fcae69162c903e1d
sal <741811875@qq.com>
2019-08-09 10:23:55 +0800

    把本地工程添加到git

--- Test-git/src/test/hi.java
+++ Test-git/src/test/hi.java
@@ -3,5 +3,9 @@ package test;
 public class hi {
     public static void main(String[] args) {
         System.out.println("helloworld");
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("hello");
+        }
     }
 }
